900c081d23ae076adfe75a3f0cb94563eadb2cfc
BriceVaillant <naw.brice@gmail.com>
2026-02-17 12:02:15 +0100

    solved 181 challenge

--- 181_Best_Time_To_Buy_And_Sell_Stock.rb
+++ 181_Best_Time_To_Buy_And_Sell_Stock.rb
@@ -1,11 +1,23 @@
 def max_profit(prices)
-    
+  max_profit = 0
+  min_price = prices[0]
+
+  (1...prices.length).each do |k|
+    if prices[k] < min_price
+      min_price = prices[k]
+    elsif max_profit < prices[k] - min_price
+      max_profit = prices[k] - min_price
+    end
+  end
+  return max_profit
 end
 
-prices1 = [7,1,5,3,6,4]
-prices2 = [7,6,4,3,1]
+prices1 = [7, 1, 5, 3, 6, 4]
+prices2 = [7, 6, 4, 3, 1]
+prices3 = [1, 2]
 p(max_profit(prices1))
 p(max_profit(prices2))
+p(max_profit(prices3))
 
 # Input: prices = [7,1,5,3,6,4]
 # Output: 5
